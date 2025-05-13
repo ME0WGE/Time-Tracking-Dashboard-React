2490d66dd266e56b32131554e5bd5bd9452276b8
Kamil Baldyga <baldygakamil@gmail.com>
2025-05-13 10:28:40 +0200

    add activity card body - activity-title div

--- src/components/activityCard/ActivityCard.jsx
+++ src/components/activityCard/ActivityCard.jsx
@@ -2,11 +2,23 @@ import "./activityCard.css";
 
 export default function ActivityCard({ imageSrc }) {
   const imageSource = `src/assets/images/${imageSrc}`;
-  const altText = "image of" + imageSrc;
+  const altText = "image of " + imageSrc;
+
+  const activityTitle = "Work";
+  const ellipsisIcon = `src/assets/images/icon-ellipsis.svg`;
+  const ellipsisAltText = "image of ellipsis icon";
+
   return (
     <>
       <div className="activity-card">
         <img src={imageSource} alt={altText} />
+        <div className="activity-card-body">
+          <div className="activity-title">
+            {activityTitle}
+            <img src={ellipsisIcon} alt={ellipsisAltText} />
+          </div>
+          <div className="activity-timeframes"></div>
+        </div>
       </div>
     </>
   );
